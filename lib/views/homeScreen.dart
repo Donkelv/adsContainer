c428613328a8a746604fc745292db45f37a0ac24
Donkelv <kevinohiro@gmail.com>
2021-06-06 14:31:51 -0700

    complete design

---
+++ lib/views/homeScreen.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blueGrey,
+        title: Text("Hello World", style: TextStyle(fontSize: 14.0, color: Colors.white,),),
+        leadingWidth: 0.0,
+        centerTitle: false,
+      ),
+      body: Container(
+        color: Colors.white,
+        height: size.height,
+        width: size.width,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Padding(
+              padding:  EdgeInsets.symmetric(horizontal: size.width * 0.1),
+              child: Container(
+                width: size.width,
+                height: size.height * 0.1,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(5.0),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Color(0xFFD3D4DA),
+                      blurRadius: 7.0,
+                      spreadRadius: 2.0,
+                      offset: Offset(0.0, 6.0),
+                   ),
+                  ],
+                ),
+              ),
+            ),
+            SizedBox(
+              height: size.height * 0.05,
+            ),
+            Text("Hello World")
+
+          ],
+        ),
+      ),
+      
+    );
+  }
+}
